52223d0dafb00e73536b1fa98249e7817b37f64e
Pouya Rostam <pooya.rostam@gmail.com>
2020-04-09 07:45:36 -0700

    Add apps

---
+++ apps/install.sh
@@ -0,0 +1,19 @@
+
+function install {
+  which $1 #&> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    apt-get install $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install vim
+#install ctags
+install tmux
+#install dtach
+#install dstat
+#install nmap
+#install silversearcher-ag
